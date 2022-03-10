3922b115189042f706cc205d6854eea0a72f4b68
tratium <trey@tratium.net>
2022-03-10 12:01:56 -0700

    add dropdown nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,19 +7,23 @@ export function Header() {
 
     <Navbar bg="light" expand="lg">
   <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Brand href="/">Toke Wars</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-      <Nav.Link href="/">Home</Nav.Link>
+{/*       <Nav.Link href="/reactors">Reactors</Nav.Link>
+      <Nav.Link href="/leaderboard">Leaderboard</Nav.Link>
+      <Nav.Link href="/stages">Liquidity Stages</Nav.Link> */}
+      <NavDropdown title="DAOs" id="basic-nav-dropdown">
         {DAOS.map(([_, name]) => (
-          <Nav.Link className="me-auto" key={name}>
-            <NavLink  to={`daos/${name}`} key={name}>
-              {name}
-            </NavLink>
-          </Nav.Link>
-        ))}
-      </Nav>
+            <NavDropdown.Item className="me-auto" key={name}>
+              <NavLink  to={`daos/${name}`} key={name}>
+                {name}
+              </NavLink>
+            </NavDropdown.Item>
+          ))}
+        </NavDropdown>
+        </Nav>
     </Navbar.Collapse>
   </Container>
 </Navbar>
