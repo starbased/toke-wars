2f428fe80f8f2e49873d41de09d0d24df357ab98
Loblolly <loblolly690@gmail.com>
2022-03-10 10:39:17 -0500

    Make chart stack

--- src/components/TokeChart.tsx
+++ src/components/TokeChart.tsx
@@ -1,12 +1,12 @@
 import {
-  LineChart,
-  Line,
+  AreaChart,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
   Legend,
   ResponsiveContainer,
+  Area,
 } from "recharts";
 import { useAmounts } from "./Dao";
 import { sortBy } from "lodash";
@@ -22,50 +22,79 @@ export function TokeChart({ address }: { address: string }) {
 
   let data = [
     ...tTokeEvents.map(({ time, total }) => ({
-      time: time.getTime() + 1, //todo: weird hack to get the graph to work, they need to be merged so that duplicate times don't exist
+      time: time,
       tToke: parseInt(total),
     })),
     ...tokeEvents.map(({ time, total }) => ({
-      time: time.getTime(),
+      time: time,
       toke: parseInt(total),
     })),
-    { time: new Date().getTime() },
   ];
-
   data = sortBy(data, "time");
+
+  if (data.length < 1) {
+    return <div>No data</div>;
+  }
+
+  let joinedData = [];
+  let lastDatum = { time: new Date(0), tToke: 0, toke: 0 };
+  for (let datum of data) {
+    if (lastDatum.time.getTime() == datum.time.getTime()) {
+      if ("toke" in datum) {
+        lastDatum.toke = datum.toke;
+      } else if ("tToke" in datum) {
+        lastDatum.tToke = datum.tToke;
+      }
+    } else {
+      lastDatum = { ...lastDatum, ...datum };
+      joinedData.push(lastDatum);
+    }
+  }
+
+  joinedData.push({ ...joinedData[joinedData.length - 1], time: new Date() });
+
+  const dateFormatter = (date: Date) => date.toLocaleDateString("en-US");
 
   return (
     <div style={{ width: "1000px", height: "400px" }}>
       <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          // width={500}
-          // height={300}
-          data={data}
+        <AreaChart
+          data={joinedData}
           margin={{
-            top: 50,
+            top: 0,
             right: 30,
             left: 20,
             bottom: 5,
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="time" scale="time" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line
-            connectNulls={true}
-            type="stepAfter"
-            dataKey="toke"
-            stroke="#82ca9d"
+          <XAxis
+            dataKey="time"
+            scale="time"
+            type="number"
+            tickFormatter={dateFormatter}
+            domain={[() => data[0].time, () => new Date()]}
           />
-          <Line
+          <YAxis />
+          <Tooltip labelFormatter={dateFormatter} />
+          <Legend />
+          <Area
             connectNulls={true}
             type="stepAfter"
             dataKey="tToke"
             stroke="#8884d8"
+            fill="#8884d8"
+            stackId="1"
           />
-        </LineChart>
+          <Area
+            connectNulls={true}
+            type="stepAfter"
+            dataKey="toke"
+            stroke="#82ca9d"
+            fill="#82ca9d"
+            stackId="1"
+          />
+        </AreaChart>
       </ResponsiveContainer>
     </div>
   );
