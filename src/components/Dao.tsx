845ecb346b1b33f9163a4d65dde39db1a1f35219
tratium <trey@tratium.net>
2022-03-11 12:25:58 -0700

    add link to Medium Post

--- src/components/Dao.tsx
+++ src/components/Dao.tsx
@@ -2,11 +2,12 @@ import { TokeChart } from "./TokeChart";
 import { DaoInformation, T_TOKE_CONTRACT, TOKE_CONTRACT } from "../constants";
 import { useAmounts } from "../api/Erc20";
 import { useNewStaking } from "../api/TokeStaking";
-import { Center, Divider } from "@chakra-ui/react";
+import { Button, Center, Divider, Text } from "@chakra-ui/react";
 import { Container, VStack } from "@chakra-ui/react";
 import { DaoDetailsCard } from "./DaoDetailsCard";
 import BigNumber from "bignumber.js";
 import { addDays } from "date-fns";
+import { FcGoogle } from "react-icons/fc";
 
 function getAmount(array: { total: string; time: Date }[][]) {
   return (
