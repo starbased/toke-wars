718292f9a74dda4c23512c8f985671b46eea74da
Loblolly <loblolly690@gmail.com>
2022-03-11 09:33:46 -0500

    Refactor cards to use base component

--- src/components/DaoDetailsCard.tsx
+++ src/components/DaoDetailsCard.tsx
@@ -1,10 +1,8 @@
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import {
-  Badge,
   Box,
   chakra,
   Flex,
-  Heading,
   Link,
   SimpleGrid,
   Stat,
@@ -16,55 +14,62 @@ import {
   useColorModeValue,
 } from "@chakra-ui/react";
 import { ReactNode } from "react";
-import { BsPerson } from "react-icons/bs";
-import { FiServer } from "react-icons/fi";
-import { GoLocation } from "react-icons/go";
+
+type BaseCardProps = {
+  title: string;
+  children: ReactNode;
+};
+
+function BaseCard({ title, children }: BaseCardProps) {
+  return (
+    <Stat
+      px={{ base: 2, md: 4 }}
+      py="5"
+      shadow="xl"
+      border="1px solid"
+      borderColor={useColorModeValue("gray.800", "gray.500")}
+      rounded="lg"
+    >
+      <Flex justifyContent="space-between">
+        <Box pl={{ base: 2, md: 4 }}>
+          <StatLabel fontWeight="medium" isTruncated>
+            {title}
+          </StatLabel>
+          <StatGroup>{children}</StatGroup>
+        </Box>
+        <Box
+          my="auto"
+          color={useColorModeValue("gray.800", "gray.200")}
+          alignContent="center"
+        >
+          {/* {icon} */}
+        </Box>
+      </Flex>
+    </Stat>
+  );
+}
 
 interface StatsCardProps {
   title: string;
   total: number;
   /* icon: ReactNode; */
 }
-function StatsCard(props: StatsCardProps) {
-  const { title, total } = props;
+function StatsCard({ title, total }: StatsCardProps) {
   return (
-    <Stat
-      px={{ base: 2, md: 4 }}
-      py={"5"}
-      shadow={"xl"}
-      border={"1px solid"}
-      borderColor={useColorModeValue("gray.800", "gray.500")}
-      rounded={"lg"}
-    >
-      <Flex justifyContent={"space-between"}>
-        <Box pl={{ base: 2, md: 4 }}>
-          <StatLabel fontWeight={"medium"} isTruncated>
-            {title}
-          </StatLabel>
-          <StatGroup>
-            <Stat>
-              <StatNumber>{total}</StatNumber>
-              <StatHelpText>
-                <StatArrow type="increase" />
-                {/* 
+    <BaseCard title={title}>
+      <Stat>
+        <StatNumber>{total}</StatNumber>
+        <StatHelpText>
+          <StatArrow type="increase" />
+          {/* 
                 get percent increase
                 if newStaking contains sum totals,
                 can compare latest to previous
                 */}
-                {(((total - 10000) / total) * 100).toFixed(2)}%
-              </StatHelpText>
-            </Stat>
-          </StatGroup>
-        </Box>
-        <Box
-          my={"auto"}
-          color={useColorModeValue("gray.800", "gray.200")}
-          alignContent={"center"}
-        >
-          {/* {icon} */}
-        </Box>
-      </Flex>
-    </Stat>
+          {(((total - 10000) / total) * 100).toFixed(2)}%
+        </StatHelpText>
+      </Stat>
+    </BaseCard>
   );
 }
 
@@ -73,39 +78,16 @@ interface StageCardProps {
   stage: string;
   /* icon: ReactNode; */
 }
-function StageCard(props: StageCardProps) {
-  const { title, stage } = props;
+function StageCard({ title, stage }: StageCardProps) {
   return (
-    <Stat
-      px={{ base: 2, md: 4 }}
-      py={"5"}
-      shadow={"xl"}
-      border={"1px solid"}
-      borderColor={useColorModeValue("gray.800", "gray.500")}
-      rounded={"lg"}
-    >
-      <Flex justifyContent={"space-between"}>
-        <Box pl={{ base: 2, md: 4 }}>
-          <StatLabel fontWeight={"medium"} isTruncated>
-            {title}
-          </StatLabel>
-
-          <StatGroup>
-            <Stat>
-              <StatNumber>{stage}</StatNumber>
-              <StatHelpText>Stage {stage}: Incentivized Liquidity via Emissions</StatHelpText>
-            </Stat>
-          </StatGroup>
-        </Box>
-        <Box
-          my={"auto"}
-          color={useColorModeValue("gray.800", "gray.200")}
-          alignContent={"center"}
-        >
-          {/* {icon} */}
-        </Box>
-      </Flex>
-    </Stat>
+    <BaseCard title={title}>
+      <Stat>
+        <StatNumber>{stage}</StatNumber>
+        <StatHelpText>
+          Stage {stage}: Incentivized Liquidity via Emissions
+        </StatHelpText>
+      </Stat>
+    </BaseCard>
   );
 }
 
@@ -114,47 +96,18 @@ interface StatsLinkCardProps {
   addresses: string[];
   /* icon: ReactNode; */
 }
-function StatsLinkCard(props: StatsLinkCardProps) {
-  const { title, addresses } = props;
+function StatsLinkCard({ title, addresses }: StatsLinkCardProps) {
   return (
-    <Stat
-      px={{ base: 2, md: 4 }}
-      py={"5"}
-      shadow={"xl"}
-      border={"1px solid"}
-      borderColor={useColorModeValue("gray.800", "gray.500")}
-      rounded={"lg"}
-    >
-      <Flex justifyContent={"space-between"}>
-        <Box pl={{ base: 2, md: 4 }}>
-          <StatLabel fontWeight={"medium"} isTruncated>
-            {title}
-          </StatLabel>
-          <StatGroup>
-            <Stat>
-              <StatNumber>
-                <Link
-                  href={"http://zapper.fi/account/" + addresses[0]}
-                  isExternal
-                >
-                  Current Treasury <ExternalLinkIcon mx="2px" />
-                </Link>
-              </StatNumber>
-              <StatHelpText>{addresses[0].substring(0,5)}</StatHelpText>
-            </Stat>
-          </StatGroup>
-
-          <StatNumber fontSize={"2xl"} fontWeight={"medium"}></StatNumber>
-        </Box>
-        <Box
-          my={"auto"}
-          color={useColorModeValue("gray.800", "gray.200")}
-          alignContent={"center"}
-        >
-          {/* {icon} */}
-        </Box>
-      </Flex>
-    </Stat>
+    <BaseCard title={title}>
+      <Stat>
+        <StatNumber>
+          <Link href={"http://zapper.fi/account/" + addresses[0]} isExternal>
+            Current Treasury <ExternalLinkIcon mx="2px" />
+          </Link>
+        </StatNumber>
+        <StatHelpText>{addresses[0].substring(0, 5)}</StatHelpText>
+      </Stat>
+    </BaseCard>
   );
 }
 
@@ -167,13 +120,8 @@ type Props = {
 
 export function DaoDetailsCard({ name, addresses, stage, total }: Props) {
   return (
-    <Box maxW="7xl" mx={"auto"} pt={5} px={{ base: 2, sm: 12, md: 17 }}>
-      <chakra.h1
-        textAlign={"center"}
-        fontSize={"4xl"}
-        py={10}
-        fontWeight={"bold"}
-      >
+    <Box maxW="7xl" mx="auto" pt={5} px={{ base: 2, sm: 12, md: 17 }}>
+      <chakra.h1 textAlign="center" fontSize="4xl" py={10} fontWeight="bold">
         {name}
       </chakra.h1>
       {/*         <Heading as='h4' style={{ display: "flex", gap: "5px" }}>
@@ -185,17 +133,17 @@ export function DaoDetailsCard({ name, addresses, stage, total }: Props) {
         </Heading> */}
       <SimpleGrid columns={{ base: 1, md: 3 }} spacing={{ base: 5, lg: 8 }}>
         <StatsCard
-          title={"Total TOKE Owned"}
+          title="Total TOKE Owned"
           total={total}
-          /* icon={"┻┳"} */
+          /* icon="┻┳" */
         />
         <StageCard
-          title={"DAO Stage"}
+          title="DAO Stage"
           stage={stage}
           /* icon={<FiServer size={'3em'} />} */
         />
         <StatsLinkCard
-          title={"Zapper"}
+          title="Zapper"
           addresses={addresses}
           /* icon={<GoLocation size={'3em'} />} */
         />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,7 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 import {
   Box,
   Flex,
-  Avatar,
   HStack,
   Link,
   IconButton,
@@ -11,27 +10,27 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
-  MenuDivider,
   useDisclosure,
   useColorModeValue,
   Stack,
-} from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
-import { LinkContainer } from 'react-router-bootstrap';
-import { DAOS } from '../constants';
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { LinkContainer } from "react-router-bootstrap";
+import { DAOS } from "../constants";
 
-const Links = ['Reactors', 'Leaderboard', 'Liquidity Stages'];
+const Links = ["Reactors", "Leaderboard", "Liquidity Stages"];
 
 const NavLink = ({ children }: { children: ReactNode }) => (
   <Link
     px={2}
     py={1}
-    rounded={'md'}
+    rounded="md"
     _hover={{
-      textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
     }}
-    href={'#'}>
+    href="#"
+  >
     {children}
   </Link>
 );
@@ -41,36 +40,29 @@ export function Header() {
 
   return (
     <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems="center" justifyContent="space-between">
           <IconButton
-            size={'md'}
+            size="md"
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
+            aria-label="Open Menu"
+            display={{ md: "none" }}
             onClick={isOpen ? onClose : onOpen}
           />
-          <HStack spacing={8} alignItems={'center'}>
+          <HStack spacing={8} alignItems="center">
             <Box>Toke Wars</Box>
-            <HStack
-              as={'nav'}
-              spacing={4}
-              display={{ base: 'none', md: 'flex' }}>
+            <HStack as="nav" spacing={4} display={{ base: "none", md: "flex" }}>
               {Links.map((link) => (
                 <NavLink key={link}>{link}</NavLink>
               ))}
               <Menu>
-                <MenuButton
-                  as={Button}
-                  minW={0}>
-                    DAOs
+                <MenuButton as={Button} minW={0}>
+                  DAOs
                 </MenuButton>
                 <MenuList>
                   {DAOS.map(({ name }) => (
                     <LinkContainer to={`daos/${name}`} key={name}>
-                      <MenuItem key={name}>
-                        {name}
-                      </MenuItem>
+                      <MenuItem key={name}>{name}</MenuItem>
                     </LinkContainer>
                   ))}
                 </MenuList>
@@ -80,8 +72,8 @@ export function Header() {
         </Flex>
 
         {isOpen ? (
-          <Box pb={4} display={{ md: 'none' }}>
-            <Stack as={'nav'} spacing={4}>
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as="nav" spacing={4}>
               {Links.map((link) => (
                 <NavLink key={link}>{link}</NavLink>
               ))}
