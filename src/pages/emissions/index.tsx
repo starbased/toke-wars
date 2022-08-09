4182b01357bc827b14094abbf774fa9179a93876
Loblolly <loblolly690@gmail.com>
2022-08-09 17:16:46 -0600

    Add usd value to emissions page. Write a little blurb about where the data comes from

--- src/pages/emissions/index.tsx
+++ src/pages/emissions/index.tsx
@@ -3,7 +3,7 @@ import { GetStaticProps } from "next";
 import { prisma } from "../../util/db";
 import {
   Area,
-  AreaChart,
+  ComposedChart,
   CartesianGrid,
   Label,
   Legend,
@@ -11,6 +11,7 @@ import {
   Tooltip,
   XAxis,
   YAxis,
+  Line,
 } from "recharts";
 
 import { orderBy } from "lodash";
@@ -19,6 +20,11 @@ import { formatEther } from "ethers/lib/utils";
 import { Payload } from "recharts/types/component/DefaultTooltipContent";
 import { formatNumber } from "../../util/maths";
 import { useHiddenLabels } from "../../hooks/useHiddenLabel";
+import { getHistoricalPrice } from "../../util/api/coinGecko";
+import { addDays } from "date-fns";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+
+import { Link } from "@chakra-ui/react";
 
 type Total = {
   cycle: number;
@@ -27,7 +33,7 @@ type Total = {
 };
 
 type Props = {
-  data: { cycle: number }[];
+  data: { cycle: number; total: number; usdValue: number }[];
 };
 
 export default function Leaderboard({ data }: Props) {
@@ -35,6 +41,7 @@ export default function Leaderboard({ data }: Props) {
 
   const orderedEmissions = orderBy(
     Object.entries(data[data.length - 1])
+      .filter(([name]) => !["total", "usdValue"].includes(name))
       .map(([name, total]) => ({
         name,
         total,
@@ -50,7 +57,7 @@ export default function Leaderboard({ data }: Props) {
     <Page header="Reward Emissions">
       <div style={{ width: "100%", height: "500px" }}>
         <ResponsiveContainer width="100%" height="100%">
-          <AreaChart
+          <ComposedChart
             data={data}
             margin={{
               top: 0,
@@ -69,22 +76,33 @@ export default function Leaderboard({ data }: Props) {
                 (v, i) => i * 2 + 201
               )}
             />
-            <YAxis tickFormatter={(tick) => yformatter.format(tick)}>
+            <YAxis
+              tickFormatter={(tick) => yformatter.format(tick)}
+              yAxisId="right"
+              orientation="right"
+            >
               <Label
                 style={{ fill: "ghostwhite" }}
                 value="Toke"
                 angle={-90}
                 offset={10}
-                position="left"
+                position="right"
               />
             </YAxis>
+            <YAxis
+              tickFormatter={(tick) => yformatter.format(tick)}
+              unit="$"
+              yAxisId="left"
+              orientation="left"
+            ></YAxis>
+
             <Tooltip
               labelStyle={{ color: "black" }}
               labelFormatter={(label, payload: Payload<number, string>[]) => {
-                const total = payload
-                  .map((obj) => obj.value || 0)
-                  .reduce((a, b) => a + b, 0);
-                return `Cycle ${label} (${formatNumber(total, 2)})`;
+                return `Cycle ${label} (${formatNumber(
+                  payload[0]?.payload?.total,
+                  2
+                )})`;
               }}
               formatter={(amount: number) => formatNumber(amount, 2)}
             />
@@ -98,10 +116,49 @@ export default function Leaderboard({ data }: Props) {
                 stackId="1"
                 fill={graphColors[i % graphColors.length]}
                 stroke={graphColors[i % graphColors.length]}
+                yAxisId="right"
               />
             ))}
-          </AreaChart>
+
+            <Line
+              connectNulls={true}
+              dataKey="usdValue"
+              name="USD Value"
+              stroke="#8884d8"
+              yAxisId="left"
+              dot={false}
+            />
+          </ComposedChart>
         </ResponsiveContainer>
+      </div>
+      <div style={{ alignSelf: "flex-end", color: "gray" }}>
+        Price data from{" "}
+        <a href="https://www.coingecko.com" target="_blank" rel="noreferrer">
+          CoinGecko
+        </a>
+      </div>
+      <div>
+        <p>
+          This displays information about toke rewards that can be claimed each
+          cycle, not the amount that has been claimed.
+        </p>
+        <p>
+          Data is pulled from ipfs. Read more about how rewards work in the{" "}
+          <Link
+            href="https://docs.tokemak.xyz/toke/liquidity-direction/claiming-rewards-from-contract"
+            isExternal
+          >
+            tokemak docs <ExternalLinkIcon mx="2px" />
+          </Link>
+        </p>
+        <p style={{ marginTop: "15px" }}>
+          USD Value is calculated taking the total for a cycle and multiplying
+          it by the price of toke on the day of the cycle.
+        </p>
+        <p>
+          For cycles that have not occurred yet the last known price data is
+          used.
+        </p>
       </div>
     </Page>
   );
@@ -165,10 +222,31 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
     {}
   );
 
-  const data = Object.entries(byCycle).map(([k, v]) => ({
-    cycle: parseInt(k),
-    ...v,
-  }));
+  const cycle_201_time = 1645635600;
+
+  const startDate = new Date(cycle_201_time * 1000);
+
+  const historicalPrices = await getHistoricalPrice("tokemak", cycle_201_time);
+  let days = Object.keys(historicalPrices);
+
+  const data = Object.entries(byCycle).map(([cycle, v], i) => {
+    const total = Object.values(v).reduce((a, b) => a + b, 0);
+    const date = addDays(startDate, (i + 1) * 7);
+
+    const priceOnDay =
+      historicalPrices[
+        days.find((s) => date.getTime() < parseInt(s)) || days[days.length - 1]
+      ];
+
+    let usdValue = total * priceOnDay;
+
+    return {
+      cycle: parseInt(cycle),
+      total,
+      usdValue,
+      ...v,
+    };
+  });
 
   return {
     props: {
